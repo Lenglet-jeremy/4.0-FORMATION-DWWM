317fa1c4844d33a7fc368095c6e758f57a004721
Lenglet-jeremy <lengletjeremy.pro@gmail.com>
2024-04-16 16:26:43 +0200

    mise à jour React

---
+++ 5.React/composant-reactif/src/App.jsx
@@ -0,0 +1,15 @@
+
+function App() {
+
+  return (
+    <>
+    <div className={`d-flex flex-column justify-content-center align-items-center mh-100`}>
+      <input type="text" className="mb-20" />
+      <button className="btn btn-primary mb-20">Submit</button>
+    </div>
+      
+    </>
+  )
+}
+
+export default App
