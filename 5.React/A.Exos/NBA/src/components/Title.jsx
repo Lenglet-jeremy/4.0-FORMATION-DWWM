dd00d61fe7121260473f9fe412d11a1b7d5c33f9
Lenglet-jeremy <146825842+Lenglet-jeremy@users.noreply.github.com>
2024-04-18 20:56:20 +0200

    Mise à jour NBA

--- 5.React/A.Exos/NBA/src/components/Title.jsx
+++
@@ -1,7 +0,0 @@
-export default function Title(){
-    return (
-        <>
-            <h1 style={{ color: "gold" }} >NBA</h1>
-        </>
-    );
-}
--- 5.React/A.Exos/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import "./assets/styles/index.scss"
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
