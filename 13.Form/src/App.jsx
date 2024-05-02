e0c731c8c238e066e977d951c6d6cb7dd21dee64
Lenglet-jeremy <146825842+Lenglet-jeremy@users.noreply.github.com>
2024-05-02 13:23:51 +0200

    Add files via upload

---
+++ 13.Form/src/App.jsx
@@ -0,0 +1,17 @@
+import UseForm from "./components/1-UseForm";
+import Yup from "./components/2-Yup";
+import CheckboxRadioSelect from "./components/3-CheckboxRadioSelect";
+import ChampsDynamiques from "./components/4-ChampsDynamiques";
+
+function App() {
+  return (
+    <>
+      {/* <UseForm /> */}
+      {/* <Yup /> */}
+      {/* <CheckboxRadioSelect /> */}
+      <ChampsDynamiques />
+    </>
+  );
+}
+
+export default App;
